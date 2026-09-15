61f2c1c71b57d8c09c06f047256808bdf9110b9b
hohasic <hohasic@gmail.com>
2026-09-15 09:53:32 +0900

    20260915

--- calendar/src/main/java/com/office/calendar/HomeController.java
+++ calendar/src/main/java/com/office/calendar/HomeController.java
@@ -1,16 +1,19 @@
 package com.office.calendar;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
-    final private String CLASS_NAME = "[HomeController] ";
+//    final private String CLASS_NAME = "[HomeController] ";
 
     @GetMapping({"", "/"})
     public String home() {
-        System.out.println(CLASS_NAME.concat("home()"));
+//        System.out.println(CLASS_NAME.concat("home()"));
+        log.info("home()");
 
         String nextPage = "home";
 
